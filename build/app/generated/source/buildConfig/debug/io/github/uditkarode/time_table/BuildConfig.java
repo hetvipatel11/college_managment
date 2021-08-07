42bbad92303ec7607e18d232236c4521fb4654ff
hetvipatel11 <56166476+hetvipatel11@users.noreply.github.com>
2021-08-07 18:18:04 +0530

    Add files via upload

---
+++ build/app/generated/source/buildConfig/debug/io/github/uditkarode/time_table/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.github.uditkarode.time_table;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.github.uditkarode.time_table";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
